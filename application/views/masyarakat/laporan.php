ad9b479bde9e6eb10a518d7186eb9bf5a158b1ba
nasyihulwan <muhnasyihulwan@gmail.com>
2023-03-17 08:18:51 +0700

    update

--- application/views/masyarakat/laporan.php
+++ application/views/masyarakat/laporan.php
@@ -497,7 +497,7 @@
                                                     <div class="modal-content">
                                                         <div class="modal-header">
                                                             <h5 class="modal-title" id="exampleModalScrollableTitle">
-                                                                HE
+                                                                <?= $l->judul_laporan ?> - Alasan Ditolak
                                                             </h5>
                                                             <button type="button" class="close" data-bs-dismiss="modal"
                                                                 aria-label="Close">
